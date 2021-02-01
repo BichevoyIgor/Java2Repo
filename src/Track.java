97f5ecc5be8dab08abc65a6719d4fc46049ecd89
BichevoyIgor <igoreek@mail.ru>
2021-02-01 22:35:10 +0300

    убрал лишние гетеры

--- src/Track.java
+++ src/Track.java
@@ -2,9 +2,4 @@ public class Track extends Barrier{
     public Track(int barrierSize) {
         super(barrierSize);
     }
-
-    @Override
-    public int getBarrierSize() {
-        return super.getBarrierSize();
-    }
 }
--- src/Wall.java
+++ src/Wall.java
@@ -2,9 +2,4 @@ public class Wall extends Barrier{
     public Wall(int barrierSize) {
         super(barrierSize);
     }
-
-    @Override
-    public int getBarrierSize() {
-        return super.getBarrierSize();
-    }
 }
