d1e52603049535af276420df632a1e840ba5bd9f
BichevoyIgor <igoreek@mail.ru>
2021-02-02 08:39:45 +0300

    исправил рабочие часы

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     }
 
     static String getWorkingHours(Enum day){
-        if (day == DayOfWeek.MONDAY)  return "200";
-        if (day == DayOfWeek.TUESDAY)  return "160";
-        if (day == DayOfWeek.WEDNESDAY)  return "120";
-        if (day == DayOfWeek.THURSDAY)  return "80";
-        if (day == DayOfWeek.FRIDAY)  return "40";
+        if (day == DayOfWeek.MONDAY)  return "40";
+        if (day == DayOfWeek.TUESDAY)  return "32";
+        if (day == DayOfWeek.WEDNESDAY)  return "24";
+        if (day == DayOfWeek.THURSDAY)  return "16";
+        if (day == DayOfWeek.FRIDAY)  return "8";
         if (day == DayOfWeek.SATURDAY)  return "Сегодня выходной";
         if (day == DayOfWeek.SUNDAY)  return "Сегодня выходной";
         else return null;
